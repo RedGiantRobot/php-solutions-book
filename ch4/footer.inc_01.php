78336f7915a7477f43650d19718507f4b2ae8f58
Rob Nale <robnale@gmail.com>
2013-03-12 17:29:43 -0400

    Added Automatically Updating Copyright

--- ch4/footer.inc_01.php
+++
@@ -1,3 +0,0 @@
-<div id="footer">
-  <p>&copy; 2006&#8211;2010 David Powers</p>
-</div>
--- ch4/includes/footer.inc.php
+++ ch4/includes/footer.inc.php
@@ -1,3 +1,15 @@
 <div id="footer">
-    <p>&copy; 2006 David Powers</p>
-    </div>
+    <p>&copy; 
+        <?php 
+        $startYear = 2013;
+        $currentYear = date('Y');
+        if($currentYear == $startYear){
+            echo $startYear;
+        }else{
+            echo "{$startYear}&#8211;{$currentYear}";
+        }
+
+
+        ?> 
+        David Powers</p>
+</div>
