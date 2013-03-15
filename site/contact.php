5291d9d79b0f8953d4ec2b8cbd1b034d931f11ec
Rob Nale <robnale@gmail.com>
2013-03-15 17:40:44 -0400

    Added Ch5 Contact page

--- site/contact.php
+++ site/contact.php
@@ -3,18 +3,18 @@
 <html>
 <head>
 <meta charset=utf-8">
-<title>Japan Journey<?php if (isset($title)) {echo "&#8212;{$title}";} ?></title>
+<title>Japan Journey<?php echo "&#8212;{$title}"; ?></title>
 <link href="styles/journey.css" rel="stylesheet" type="text/css" media="screen">
 </head>
 
 <body>
 <div id="header">
-    <h1>Japan Journey </h1>
-</div>  
+    <h1>Japan Journey</h1>
+</div>
 <div id="wrapper">
     <?php include('./includes/menu.inc.php'); ?>
     <div id="maincontent">
-        <h2>Contact Us  </h2>
+        <h2>Contact Us </h2>
       <p>Ut enim ad minim veniam, quis nostrud exercitation consectetur adipisicing elit. Velit esse cillum dolore ullamco laboris nisi in reprehenderit in voluptate. Mollit anim id est laborum. Sunt in culpa duis aute irure dolor excepteur sint occaecat.</p>
         <form id="feedback" method="post" action="">
             <p>
@@ -32,6 +32,9 @@
             <p>
                 <input name="send" id="send" type="submit" value="Send message">
             </p>
+            <pre>
+            <?php if ($_POST) {print_r($_POST); } ?>
+            </pre>
         </form>
     </div>
     <?php include('./includes/footer.inc.php'); ?>
